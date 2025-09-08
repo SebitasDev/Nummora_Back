dd41607a7ea30c8e4c6fb4828633e470cb3898e0
SebasDev <sebastian.ramirez@blackbirdlabs.com.co>
2025-09-08 16:05:55 -0500

    Se cambio el path del endpoint de financiar un prestamo

--- src/blockchain/loan/loan.controller.ts
+++ src/blockchain/loan/loan.controller.ts
@@ -1,18 +1,26 @@
-import {Body, Controller, HttpException, HttpStatus, Post, Res} from '@nestjs/common';
-import {LoanService} from "./loan.service";
-import {LoanDto} from "./types/loan.dto";
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { LoanService } from './loan.service';
+import { LoanDto } from './types/loan.dto';
 
 @Controller('blockchain/loan')
 export class LoanController {
-
-    constructor(private readonly loanService: LoanService) {}
-    @Post()
-    async handleRequest(@Body() payload: LoanDto) {
-        try {
-            const result = await this.loanService.createLoan(payload);
-            return { message: 'Transacción enviada correctamente ✅', txHash: result };
-        } catch (err) {
-            throw new HttpException({ error: err.message }, HttpStatus.BAD_REQUEST);
-        }
+  constructor(private readonly loanService: LoanService) {}
+  @Post('finance')
+  async financeLoan(@Body() payload: LoanDto) {
+    try {
+      const result = await this.loanService.createLoan(payload);
+      return {
+        message: 'Transacción enviada correctamente ✅',
+        txHash: result,
+      };
+    } catch (err) {
+      throw new HttpException({ error: err.message }, HttpStatus.BAD_REQUEST);
     }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {ValidationPipe} from "@nestjs/common";
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:3001', 'https://nummora-front.vercel.app'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://nummora-front.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
