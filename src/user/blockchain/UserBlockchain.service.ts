c8f441cbb7f1ad2e1d5a2f8c7bef04e1ace8e7ad
SebasDev <sebastian.ramirez@blackbirdlabs.com.co>
2025-09-25 01:02:55 -0500

    refactor: switch blockchain integration from Lisk Sepolia to Celo and add referral SDK

--- src/user/blockchain/UserBlockchain.service.ts
+++ src/user/blockchain/UserBlockchain.service.ts
@@ -9,19 +9,20 @@ import {
   Transport,
   WalletClient,
 } from 'viem';
-import { liskSepolia } from 'viem/chains';
+import { celo } from 'viem/chains';
 import { UserService } from '../user.service';
 import { NummoraLoan } from '../../abis';
 import { decodeTransactionEvent } from '../../common/helpers/decodeTransactionEvent.helper';
+import { getReferralTag, submitReferral } from '@divvi/referral-sdk';
 
 @Injectable()
 export class UserBlockchainService {
   private readonly account: Account;
-  private client: WalletClient<Transport, typeof liskSepolia, Account>;
+  private client: WalletClient<Transport, typeof celo, Account>;
   private contractAddress = process.env.NUMMORA_CORE_ADDRESS! as `0x${string}`;
   private publicClient: PublicClient = createPublicClient({
-    chain: liskSepolia,
-    transport: http(liskSepolia.rpcUrls.default.http[0]),
+    chain: celo,
+    transport: http(celo.rpcUrls.default.http[0]),
   }) as unknown as PublicClient;
 
   constructor(private readonly userService: UserService) {
@@ -30,31 +31,41 @@ export class UserBlockchainService {
     );
     this.client = createWalletClient({
       account: this.account,
-      chain: liskSepolia,
-      transport: http(liskSepolia.rpcUrls.default.http[0]),
+      chain: celo,
+      transport: http(celo.rpcUrls.default.http[0]),
     });
   }
 
   async registerLender(address: Address, signature: `0x${string}`) {
     try {
-      const findLenderBlockchain = await this.publicClient.readContract({
+      const findLenderBlockchain = (await this.publicClient.readContract({
         address: this.contractAddress,
         abi: NummoraLoan,
         functionName: 'isLender',
         args: [address],
-      }) as boolean;
+      })) as boolean;
 
       if (findLenderBlockchain) {
         throw new Error('User already registered as lender');
       }
+
+      const referralTag = getReferralTag({
+        user: this.client.account.address,
+        consumer: process.env.DIVVI_CONSUMER as `0x${string}`,
+      });
 
       const txHash = await this.client.writeContract({
         address: this.contractAddress,
         abi: NummoraLoan,
         functionName: 'registerLenderWithSignature',
         args: [signature],
+        dataSuffix: `0x${referralTag}`,
       });
 
+      const chainId = celo.id;
+      
+      await submitReferral({ txHash, chainId });
+      
       const lenderRegisterEvent =
         await decodeTransactionEvent<'LenderRegistered'>(
           this.publicClient,
@@ -79,12 +90,12 @@ export class UserBlockchainService {
 
   async registerBorrower(address: Address, signature: `0x${string}`) {
     try {
-      const findBorrowerBlockchain = await this.publicClient.readContract({
+      const findBorrowerBlockchain = (await this.publicClient.readContract({
         address: this.contractAddress,
         abi: NummoraLoan,
         functionName: 'isBorrower',
         args: [address],
-      }) as boolean;
+      })) as boolean;
 
       if (findBorrowerBlockchain) {
         throw new Error('User already registered as borrower');
